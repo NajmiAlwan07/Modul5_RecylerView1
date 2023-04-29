bae870f267ebc72ceb8c3074ad90b95c8c67e165
Najmi Alwan <najmialwan07@gmail.com>
2023-04-29 21:26:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/modul5_recylerview1/Detail.kt
@@ -0,0 +1,12 @@
+package com.example.modul5_recylerview1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.modul5_recyclerview1.R
+
+class Detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
